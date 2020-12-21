99e182c48f5fc7b6723f51cc841a46ac91f7bdc8
ClareKang <shiningkey89@gmail.com>
2020-12-21 18:55:04 +0900

    Change lib

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@bemypetkr/web-ui'
+import '@bemypetkr/web-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
