6a23640bb8742a4d08cfbb50117b65765137455a
ClareKang <shiningkey89@gmail.com>
2020-12-22 19:44:17 +0900

    Add testing

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "index";
+import React from "react";
+import renderer from "react-test-renderer";
+
 import { Button } from "./Button";
 
 describe("Button", () => {
-  it("is truthy", () => {
-    expect(Button).toBeTruthy();
+  let component: any = null;
+
+  it("renders correctly", () => {
+    component = renderer.create(
+      <ThemeProvider>
+        <Button />
+      </ThemeProvider>,
+    );
+    expect(component).toBeTruthy();
   });
 });
